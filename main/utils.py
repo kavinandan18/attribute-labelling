7163597d5bdb206f19b831719f06b32a8280626a
Puneet Dixit <puneet.dixit@statusneo.com>
2023-07-13 17:00:16 +0530

    Desc: Removed unwanted function

--- main/utils.py
+++ main/utils.py
@@ -1,12 +1,8 @@
-import operator
 from datetime import datetime
 
-from flask import request
-from flask_sqlalchemy import SQLAlchemy
-from marshmallow import Schema, ValidationError, fields
-from marshmallow.validate import Length
-from sqlalchemy import between, or_
 from bson.objectid import ObjectId
+from flask import request
+from marshmallow import ValidationError
 
 from main.exceptions import CustomValidationError
 from main.logger import INFO, get_logger
@@ -63,34 +59,9 @@ def validate_int_float_date(value: int | float | str):
         raise ValidationError(f"Value {value} must be an int, float, or str('yyyy-mm-dd')")
 
 
-class FiltersDataSchema(Schema):
-    """
-    Schema to validate filters data
-    """
-
-    eq = fields.Dict(fields.String(), fields.Field(validate=validate_not_dict_list_tuple), required=False)
-    ne = fields.Dict(fields.String(), fields.Field(validate=validate_not_dict_list_tuple), required=False)
-    lt = fields.Dict(fields.String(), fields.Field(validate=validate_int_float_date), required=False)
-    gt = fields.Dict(fields.String(), fields.Field(validate=validate_int_float_date), required=False)
-    lte = fields.Dict(fields.String(), fields.Field(validate=validate_int_float_date), required=False)
-    gte = fields.Dict(fields.String(), fields.Field(validate=validate_int_float_date), required=False)
-    between = fields.Dict(
-        fields.String(),
-        fields.List(fields.Field(validate=validate_int_float_date), validate=Length(equal=2)),
-        required=False,
-    )
-    op_in = fields.Dict(
-        fields.String(), fields.List(fields.Field(validate=validate_not_dict_list_tuple)), required=False
-    )
-    nin = fields.Dict(fields.String(), fields.List(fields.Field(validate=validate_not_dict_list_tuple)), required=False)
-    null = fields.List(fields.String(), required=False)
-    not_null = fields.List(fields.String(), required=False)
-    op_or = fields.Dict(fields.String(), fields.Field(validate=validate_not_dict_list_tuple), required=False)
-    substr = fields.Dict(fields.String(), fields.String(validate=validate_substr), required=False)
-
-
 def get_data_from_request_or_raise_validation_error(
-        validator_schema: type, data: dict, many: bool = False) -> dict or list:
+    validator_schema: type, data: dict, many: bool = False
+) -> dict or list:
     """
     This function is used to get the and validate it according to its validator schema and
     return request data in dict form. Also, it is used to raise ValidationError (A Custom
@@ -136,86 +107,3 @@ def log_user_access(response):
         f"Status code: {response.status_code}"
     )
     return response
-
-
-def add_filters_using_mapping(model: type, conditions: dict, filters: list, operator_key: str):
-    """
-    This function is used to update the filters using input and operators mapping.
-    :param model: The SQLAlchemy model to add filters to.
-    :param conditions:
-    :param filters:
-    :param operator_key:
-    :return:
-    """
-    operator_mapping = {
-        "eq": operator.eq,
-        "ne": operator.ne,
-        "lt": operator.lt,
-        "lte": operator.le,
-        "gt": operator.gt,
-        "gte": operator.ge,
-        # "contains": operator.contains,
-        # "has_key": lambda x, y: y in x,
-        # "any": any,
-        # "has_all": lambda x, y: all(elem in x for elem in y),
-    }
-
-    logical_or_filters = []
-    for column, value in conditions.items():
-        if hasattr(model, column):
-            if operator_key == "between":
-                filters.append(between(getattr(model, column), value[0], value[1]))
-            elif operator_key == "op_in":
-                filters.append(getattr(model, column).in_(value))
-            elif operator_key == "nin":
-                filters.append(getattr(model, column).notin_(value))
-            elif operator_key == "op_or":
-                logical_or_filters.append(getattr(model, column) == value)
-            elif operator_key == "substr":
-                filters.append(getattr(model, column).like(value))
-            else:
-                filters.append(operator_mapping[operator_key](getattr(model, column), value))
-    if logical_or_filters:
-        filters.append(or_(*logical_or_filters))
-
-
-def add_filters_for_null_and_not_null(model: type, operator_key: str, conditions: dict, filters: list):
-    """
-    This function is used to add filters for null and not null values.
-    :param model: The SQLAlchemy model to add filters to.
-    :param operator_key:
-    :param conditions:
-    :param filters:
-    :return:
-    """
-    for column in conditions:
-        if hasattr(model, column):
-            if operator_key == "null":
-                filters.append(getattr(model, column) == None)  # noqa
-            else:
-                filters.append(getattr(model, column) != None)  # noqa
-
-
-def get_query_including_filters(db: SQLAlchemy, model: type, filter_dict: dict):
-    """
-    This function is used to get the query with all filters
-    :param db:
-    :param model: The SQLAlchemy model to add filters to.
-    :param filter_dict:
-    :return:
-    """
-    try:
-        validator = FiltersDataSchema()
-        filter_dict = validator.load(filter_dict)
-    except ValidationError as err:
-        return err
-
-    query = db.session.query(model)
-
-    filters = []
-    for operator_key, conditions in filter_dict.items():
-        if operator_key == "null" or operator_key == "not_null":
-            add_filters_for_null_and_not_null(model, operator_key, conditions, filters)
-        else:
-            add_filters_using_mapping(model, conditions, filters, operator_key)
-    return query.filter(*filters)
