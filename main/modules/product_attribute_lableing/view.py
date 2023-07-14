f92ed8df77a13a60e07ea1c113a8a0d984665da1
Puneet Dixit <puneet.dixit@statusneo.com>
2023-07-14 11:41:03 +0530

    Desc: postman collection added

--- main/modules/product_attribute_lableing/view.py
+++ main/modules/product_attribute_lableing/view.py
@@ -17,7 +17,7 @@ from main.utils import get_data_from_request_or_raise_validation_error
 class TestServer(Resource):
     @staticmethod
     def get():
-        return make_response(jsonify(status="ok", msg="Server is running..."))
+        return make_response(jsonify(status="ok", msg="Server is running... test 1g"))
 
 
 class AttributeConfigs(Resource):
